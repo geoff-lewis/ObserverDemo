c2b65294f29c59fe7fdb00727c5d26e7bf1d9906
Geoff.Lewis <Geoff.Lewis@atmosi.com>
2021-01-26 09:42:41 +0000

    Initial commit of code for demo

---
+++ ObserverDemo/BaggageInfo.cs
@@ -0,0 +1,34 @@
+﻿namespace ObserverDemo
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BaggageInfo
+    {
+        private int flightNo;
+        private string origin;
+        private int location;
+
+        internal BaggageInfo(int flight, string from, int carousel)
+        {
+            this.flightNo = flight;
+            this.origin = from;
+            this.location = carousel;
+        }
+
+        public int FlightNumber
+        {
+            get { return this.flightNo; }
+        }
+
+        public string From
+        {
+            get { return this.origin; }
+        }
+
+        public int Carousel
+        {
+            get { return this.location; }
+        }
+    }
+}
